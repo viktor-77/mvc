7918337520a3ad2df5b4889331fa5dd22efc55d9
Viktor <vumanskiy0664041341@gmail.com>
2022-08-03 14:41:50 +0300

    task1 TestController

--- project/controllers/TestController.php
+++ project/controllers/TestController.php
@@ -4,9 +4,11 @@ namespace Project\Controllers;
 
 use \Core\Controller;
 
+//task 1
 //Сделайте контроллер TestController с действиями act1, act2 и act3.
 // Сделайте три роута, задающие адреса, по которым можно будет обратиться к этим действиям.
 // Проверьте работу созданного вами кода, по очереди обратившись через адресную строку к каждому из действий.
+
 class TestController extends Controller
 {
     public function act1()
