1a1ac22967f62aa92c3e735c9c8394aa80487419
Viktor <vumanskiy0664041341@gmail.com>
2022-08-03 14:59:35 +0300

    task 2

--- project/config/routes.php
+++ project/config/routes.php
@@ -7,5 +7,6 @@ return [
     new Route('/act1', 'test', 'act1'),
     new Route('/act2', 'test', 'act2'),
     new Route('/act3', 'test', 'act3'),
+    new Route('/nums/:n1/:n2/:n3', 'num', 'sum'),
 ];
 	
---
+++ project/controllers/NumController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Project\Controllers;
+
+//Сделайте контроллер NumController, а в нем - действие sum.
+// Пусть это действие обрабатывает адреса следующего вида: /nums/:n1/:n2/:n3/,
+// где параметрами будут некоторые числа. Сделайте так, чтобы на экран выводилась сумма переданных чисел.
+
+class NumController
+{
+    public function sum($params)
+    {
+        echo $params['n1'] + $params['n2'] + $params['n3'];
+    }
+}
